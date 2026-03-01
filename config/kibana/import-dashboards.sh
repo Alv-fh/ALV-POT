a71527507c205bd2e692fcad0fa001ca387cec9c
Álvaro <109484163+Alv-fh@users.noreply.github.com>
2026-03-01 01:13:48 +0100

    Update and rename config/kibana/dashboards/import-dashboards.sh to config/kibana/import-dashboards.sh

--- config/kibana/import-dashboards.sh
+++ config/kibana/import-dashboards.sh
@@ -5,7 +5,7 @@ until curl -s http://localhost:5601/api/status | grep -q '"level":"available"'; 
 done
 
 echo "Importando dashboards..."
-for file in ./config/kibana/*.ndjson; do
+for file in ./config/kibana/dashboards/*.ndjson; do
   curl -X POST "http://localhost:5601/api/saved_objects/_import?overwrite=true" \
     -H "kbn-xsrf: true" \
     -F "file=@$file"
